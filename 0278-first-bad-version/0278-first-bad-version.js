0a0efe2e893942daaeef43717c99c55a7908811d
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-11-12 22:42:22 +0530

    Time: 8468 ms (6.55%), Space: 42.3 MB (8.37%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        for(let a=1;a<=n;a++){
+            if(!isBadVersion(a)){
+                continue
+            }else{
+                return a
+            }
+        }
+       
+    };
+};
