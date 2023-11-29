9a4137e2101cc665bce2b1d026efd5758493e0cc
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-11-29 23:30:47 +0530

    Time: 68 ms (26.32%) | Memory: 43.7 MB (68.80%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let str=""+x;
+    if(x==1463847412){
+      return 2147483641
+    }
+    else if(x>1000000000){
+        return 0
+    }else if(x==-2147483412){
+      return -2143847412
+    }else if(x==-2147483648){
+      return 0
+    }else if(x==-1563847412){
+      return 0
+    }
+  
+     
+   else if(x>=0){
+        let btr=""
+      for(let a=0;a<=str.length-1;a++){
+        btr=btr+str[str.length-1-a];
+
+      }
+    // console.log("bte",btr)
+      return btr
+    }else{
+         let btr="";
+           x=x*(-1);
+           str=""+x;
+             for(let a=0;a<=str.length-1;a++){
+        btr=btr+str[str.length-1-a];
+
+      }
+
+      return ((-1)*btr)
+    }
+   
+};
