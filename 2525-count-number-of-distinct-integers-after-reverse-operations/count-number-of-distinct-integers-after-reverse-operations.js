97d1b09b44e4464ae5fe8d58e0bb9fdfae1300a8
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-11-30 00:22:19 +0530

    Time: 481 ms (8.93%) | Memory: 123.6 MB (5.35%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.js
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.js
@@ -2,20 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var countDistinctIntegers = function(nums) {
+// var countDistinctIntegers = function(nums) {
     
 
 
-let obj={};
+// let obj={};
 
-for(let a=0;a<=nums.length-1;a++){
-    let x=+(nums[a].toString().split('').reverse().join(''));
+// for(let a=0;a<=nums.length-1;a++){
+//     let x=+(nums[a].toString().split('').reverse().join(''));
   
    
-    obj[nums[a]]=1;
-    obj[x]=1;
-    // console.log(typeof(x))
-}
+//     obj[nums[a]]=1;
+//     obj[x]=1;
+//     // console.log(typeof(x))
+// }
 
 
 
@@ -23,5 +23,18 @@ for(let a=0;a<=nums.length-1;a++){
 
 
 
-return Object.keys(obj).length
+// return Object.keys(obj).length
+// };
+
+var countDistinctIntegers = function(nums) {
+    let n = nums.length;
+    let obj = {}
+    for(var x of nums)
+        obj[+x] = 1;
+    for(let j=0; j<n; j++)
+        {
+            let num = +(nums[j].toString().split("").reverse().join(""))
+            obj[num] =1
+        }
+    return Object.keys(obj).length
 };
