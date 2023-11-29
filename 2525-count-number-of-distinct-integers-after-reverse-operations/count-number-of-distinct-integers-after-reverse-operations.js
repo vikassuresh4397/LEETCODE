fb4457a6743d6dd50084289f322e838e0132a9b6
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-11-30 00:18:44 +0530

    Time: 520 ms (5.35%) | Memory: 146.3 MB (5.35%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.js
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.js
@@ -6,23 +6,22 @@ var countDistinctIntegers = function(nums) {
     
 let brr=[];
 
-
+let count=0,obj={};
 
 for(let a=0;a<=nums.length-1;a++){
     let x=nums[a].toString().split('').reverse().join('');
     x=parseInt(x);
     brr.push(x)
-    brr.push(nums[a])
+    brr.push(nums[a]);
+    obj[nums[a]]=1;
+    obj[x]=1;
     // console.log(typeof(x))
 }
 
-let count=0,obj={};
 
-for(let a=0;a<=brr.length-1;a++){
-    if(obj[brr[a]]==undefined){
-       obj[brr[a]]=1;
-       count++
-    }
+
+for(let key in obj){
+    count++
 }
 
 
