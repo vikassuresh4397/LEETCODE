835ef1ed6ef6395d0d1e643cff2c86582789f680
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-12-06 22:23:47 +0530

    Time: 56 ms (35.90%) | Memory: 43.9 MB (6.15%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
@@ -3,22 +3,24 @@
  * @return {number}
  */
 var totalMoney = function(n) {
-    let arr=[],sum=0;
+    let arr=[],sum=0,count=0;
     for(let a=0;a<=n-1;a++){
         if((sum!=0 && a%7==0)){
             sum=arr[a-7]+1;
-            arr.push(sum)
+            arr.push(sum);
+            count=count+sum;
         }else{
             sum=sum+1;
-            arr.push(sum)
+            arr.push(sum);
+             count=count+sum;
         }
      
 
     }
-let count=0
-    for(let a=0;a<=arr.length-1;a++){
-        count=count+arr[a]
-    }
+// let count=0
+//     for(let a=0;a<=arr.length-1;a++){
+//         count=count+arr[a]
+//     }
 
     return count
 };
