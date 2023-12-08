5fa5c0b3ed815c909c65edd3c8c39e6872539a88
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-12-09 02:58:42 +0530

    Time: 53 ms (48.31%) | Memory: 42.2 MB (18.12%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let count=0;
+     for(let a=0;a<=nums.length-1;a++){
+         for(let b=a+1;b<=nums.length-1;b++){
+             if(nums[a]==nums[b]){
+                  count++;
+             }
+         }
+     }
+
+     return count
+};
