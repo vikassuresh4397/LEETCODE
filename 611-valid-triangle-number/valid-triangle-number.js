154ba53af6f9d05af2fd91da273866ee70491ad7
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2023-12-29 11:44:57 +0530

    Time: 725 ms (38.89%) | Memory: 68.3 MB (5.56%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangleNumber = function(arr) {
+    arr.sort(function(a,b){
+        return a-b
+    });
+    let count=[];
+    for(let c=arr.length-1;c>=2;c--){
+       let b=c-1;
+       let a=0;
+       while(a<b && b<c){
+          if(arr[b]+arr[a]>arr[c]){
+              count.push(b-a);
+              b--;
+              a=0;
+          }else{
+              a++;
+          }
+       }
+    }
+
+    let sum=0;
+    for(let a=0;a<=count.length-1;a++){
+        sum=sum+count[a]
+    };
+    return sum
+};
