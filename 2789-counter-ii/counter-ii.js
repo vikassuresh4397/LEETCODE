180145f7421c064d66171ccea54a08baabfc10b8
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-01-16 12:00:26 +0530

    Time: 61 ms (50.26%) | Memory: 45.1 MB (9.91%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let x=init;
+    // console.log(init,"dd")
+    return {
+       increment :()=>{
+           x=x+1
+        return x;
+       },
+        decrement :()=>{
+       x=x-1
+        return x;
+       },
+       reset :()=>{
+           x=init
+        return x;
+       }
+    }
+   
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
