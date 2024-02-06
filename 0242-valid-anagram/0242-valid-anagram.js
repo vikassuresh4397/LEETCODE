8016f604fa98dea25a73499188129a330dbeaa60
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-02-06 13:38:05 +0530

    Time: 64 ms (81.39%), Space: 51.6 MB (16.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,29 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(arr1, arr2) {
-    let obj={};
-    for(let a=0;a<=arr1.length-1;a++){
+    let obj={}
+    for(let a=0;a<=arr1.length;a++){
         if(obj[arr1[a]]==undefined){
-            obj[arr1[a]]=1
+            obj[arr1[a]]=1;
         }else{
-            obj[arr1[a]]++
+            obj[arr1[a]]++;
         }
     }
-
-for(let a=0;a<=arr2.length-1;a++){
-     if(obj[arr2[a]]==undefined){
-            return false;
+    for(let a=0;a<=arr2.length;a++){
+         if(obj[arr2[a]]==undefined){
+           return false
         }else{
-            obj[arr2[a]]--
+            obj[arr2[a]]--;
         }
-}
-
-for(let key in obj){
-    if(obj[key]>0||obj[key]<0){
-        return false
     }
-}
-
-return true
-
+    
+    for(let key in obj){
+        if(obj[key]>=1){
+            return false
+        }
+    }
+    
+    return true
 };
