d3c7be8294331f3d2ab39dc40fc284383733b175
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-04-06 04:00:35 +0530

    Time: 53 ms (95.95%) | Memory: 50.8 MB (98.71%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(arr) {
+  
+    
+  let left=0,right=0;
+  for(let a=1;a<=arr.length-1;a++){
+      right=arr[a]+right
+  }
+  
+  if(right==left){
+      return 0
+  }
+  
+  for(let a=1;a<=arr.length-1;a++){
+      left=left+arr[a-1];
+      right=right-arr[a];
+      if(right==left){
+          return a
+      }
+  }
+  return -1
+};
