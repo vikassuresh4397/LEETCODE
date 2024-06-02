366acbfc2024de4ec98ee8f307c0de45e630f34e
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-06-02 13:49:21 +0530

    Time: 78 ms (58.70%) | Memory: 57.3 MB (37.46%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,25 +3,14 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(arr) {
-    // let f=0,l=arr.length-1;
-    // console.log(arr)
-    
-    if(arr.length%2==1){
-            for(let a=0;a<=Math.floor(arr.length/2);a++){
-        let temp=arr[a];
-        arr[a]=arr[arr.length-1-a];
-        arr[arr.length-1-a]=temp
-    }
-    
-    return arr
-    }else{
-        for(let a=0;a<=Math.floor(arr.length/2)-1;a++){
-        let temp=arr[a];
-        arr[a]=arr[arr.length-1-a];
-        arr[arr.length-1-a]=temp
-    }
-    
-    return arr
-    }
+     let f=0,l=arr.length-1;
 
+     while(l>f){
+          let temp=arr[f];
+          arr[f]=arr[l];
+         arr[l]=temp;
+         f++;
+         l--
+           }
+    return arr
 };
