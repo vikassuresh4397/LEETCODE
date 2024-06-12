a19b34c61e40b82c1255ea0a0bb293592b7e9d88
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-06-13 01:23:01 +0530

    Time: 50 ms (74.74%), Space: 48.8 MB (64.44%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,9 +2,29 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    nums.sort(function(a,b){
-        return a-b
-    })
-    return nums
+var sortColors = function(arr) {
+    let obj={};
+    for(let a=0;a<=arr.length-1;a++){
+        if(obj[arr[a]]==undefined){
+            obj[arr[a]]=1
+        }else{
+            obj[arr[a]]++;
+        }
+    }
+    let brr=[]
+    for(let key in obj){
+        if(obj[key]>1){
+            for(let a=0;a<=obj[key]-1;a++){
+                brr.push(+key)
+            }
+        }else{
+             brr.push(+key)
+              }
+    }
+    
+    for(let a=0;a<=brr.length-1;a++){
+        arr[a]=brr[a]
+    }
+    
+    return arr
 };
