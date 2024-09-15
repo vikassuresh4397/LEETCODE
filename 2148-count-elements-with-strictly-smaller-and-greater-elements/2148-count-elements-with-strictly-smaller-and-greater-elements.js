a4d8850efef2ef07769c7821c2912185b5819c79
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-09-15 19:17:59 +0530

    Time: 64 ms (23.57%), Space: 51 MB (19.75%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
@@ -7,7 +7,7 @@ var countElements = function(arr) {
 arr.sort(function(a,b){
     return a-b
 })
-// console.log(arr);
+
 let obj={};
 for(let a=0;a<=arr.length-1;a++){
     if(obj[arr[a]]==undefined){
@@ -16,23 +16,23 @@ for(let a=0;a<=arr.length-1;a++){
         obj[arr[a]]++;
     }
 }
-//  console.log(obj);
+
 let brr=[]
 for(let key in obj){
     brr.push(+key)
 }
-//  console.log(brr);
+
  brr.sort(function(a,b){
      return a-b
  })
-//   console.log(brr);
+
 let count=[]
 for(let a=1;a<=brr.length-2;a++){
     if(brr[a-1]<brr[a] && brr[a]<brr[a+1]){
         count.push(brr[a])
     }
 }
-// console.log(count);
+
 let bount=0
 for(let a=0;a<=count.length-1;a++){
     if(obj[count[a]]!==undefined){
