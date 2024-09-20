d6496a5f1ff2b503a0af01c1c2ea79e64ad99bde
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-09-20 20:16:52 +0530

    Time: 73 ms (46.97%) | Memory: 52.6 MB (82.42%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.js
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.js
@@ -9,60 +9,30 @@ var countWords = function(arr1,arr2) {
         if(obj1[arr1[a]]==undefined){
             obj1[arr1[a]]=1
         }else{ 
-          obj1[arr1[a]]++
+          obj1[arr1[a]]=+Infinity;
         }
     }
-    //  console.log("obj1",obj1)
+    //   console.log(obj1)
+     for(let a=0;a<=arr2.length-1;a++){
+         if(obj1[arr2[a]]==undefined){
+            continue;
+         }else{
+             if(obj1[arr2[a]]==0){
+                 obj1[arr2[a]]=0
+             }else{
+                   obj1[arr2[a]]++;
+             }
+            
+         }
+     }
+    //  console.log(obj1)
+    let count=0
     for(let key in obj1){
-        if(obj1[key]>1){
-            delete obj1[key]
-        }
-    }
-    // console.log("obj1",obj1)
-        let obj2={};
-    for(let a=0;a<=arr2.length-1;a++){
-        if(obj2[arr2[a]]==undefined){
-            obj2[arr2[a]]=1
-        }else{ 
-           obj2[arr2[a]]++
-        }
-    }
-    // console.log("obj2",obj2)
-    
-     for(let key in obj2){
-        if(obj2[key]>1){
-            delete obj2[key]
-        }
-    }
-   
-    //   console.log("obj2",obj2);
-      let obj3={};
-        for(let key in obj1){
-        if(obj3[key]==undefined){
-            obj3[key]=1
-        }else{
-            obj3[key]++
-        }
-    }
-    // console.log(obj3)
-     for(let key in obj2){
-        if(obj3[key]==undefined){
-            obj3[key]=1
-        }else{
-            obj3[key]++
-        }
-    }
-    
-    
-    // console.log(obj3);
-    
-    let count=0;
-    for(let key in obj3){
-        if(obj3[key]==2){
+        if(obj1[key]==2){
             count++
         }
     }
-    
     return count
+   
 
 };
