b917f632d71ef195f6025d909dc2028656022a12
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-10-02 01:46:52 +0530

    Time: 146 ms (5.01%), Space: 58.4 MB (5.27%) - LeetHub

---
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
@@ -0,0 +1,66 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var findIntersectionValues = function(arr, arr2) {
+    let obj1={};
+    for(let a=0;a<=arr.length-1;a++){
+        if(obj1[arr[a]]==undefined){
+            obj1[arr[a]]=1
+        }else{
+            obj1[arr[a]]++
+        }
+    }
+    
+    // console.log(obj1);
+    
+      let obj2={};
+    for(let a=0;a<=arr2.length-1;a++){
+        if(obj2[arr2[a]]==undefined){
+            obj2[arr2[a]]=1
+        }else{
+            obj2[arr2[a]]++
+        }
+    }
+    
+    // console.log(obj2);
+    
+   let obj3={};
+   for(let a=0;a<=arr.length-1;a++){
+        if(obj3[arr[a]]==undefined){
+            obj3[arr[a]]=[1]
+        }
+    }
+    
+    // console.log(obj3);
+    for(let a=0;a<=arr2.length-1;a++){
+        if(obj3[arr2[a]]!==undefined){
+            obj3[arr2[a]].push(1)
+        }
+    }
+    // console.log(obj3);
+    let common=[]
+    for(let key in obj3){
+        if(obj3[key].length>=2){
+            common.push(key)
+        }
+    }
+    
+    // console.log(common);
+    let first=0,second=0;
+    for(let a=0;a<=common.length-1;a++){
+        for(let key in obj1){
+            if(key==common[a]){
+                first=first+obj1[key]
+            }
+        }
+        for(let key in obj2){
+            if(key==common[a]){
+                second=second+obj2[key]
+            }
+        }
+    }
+    // console.log(first,second)
+    return [first,second]
+};
