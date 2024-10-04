74404e641f9f456cb4358c38e81e1786b27c8990
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2024-10-04 06:00:37 +0530

    Time: 49 ms (95.66%) | Memory: 52.4 MB (31.19%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let arr=[];
+    
+    for(let a=1;a<=n;a++){
+           if(a%3==0 && a%5==0){
+        arr.push("FizzBuzz")
+    }else if(a%3==0){
+         arr.push("Fizz")    
+    }else if(a%5==0){
+         arr.push("Buzz")    
+    }else {
+     a=""+a
+        arr.push(a)
+    }
+    }
+ 
+    
+    return arr
+};
