8e026bf82b2aad7ee3f14bec442fb2960e61292e
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2025-08-09 08:24:44 +0530

    Time: 9 ms (2.61%), Space: 57.7 MB (7.96%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,12 +3,23 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    for(let a=0;a<=Math.ceil(n**0.5);a++){
-        let x= 2**a;
-        if(x==n){
-            return true
-        }
+    if(n==0){
+        return false
     }
-    
+
+    if(n==1){
+        return true
+    }
+   let sum=0,rum=0
+    for(let a=1;a<=Math.ceil(n**0.5);a++){
+         let x=2**a;
+       if(x==n){
+               return true
+         
+    }
+    }
+
     return false
+
+
 };
