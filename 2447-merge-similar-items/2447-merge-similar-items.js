01bdae41d9fe957d5063a8b908ae3ae895183595
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2025-09-01 01:51:16 +0530

    Time: 148 ms (6.74%), Space: 67.6 MB (5.62%) - LeetHub

---
+++ 2447-merge-similar-items/2447-merge-similar-items.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} items1
+ * @param {number[][]} items2
+ * @return {number[][]}
+ */
+var mergeSimilarItems = function(arr1,arr2) {
+    let obj={};
+    for(let a=0;a<=arr1.length-1;a++){
+         if(obj[arr1[a][0]]==undefined){
+             obj[arr1[a][0]]=[arr1[a][1]]
+         }
+       
+    }
+     console.log(obj)
+    for(let a=0;a<=arr2.length-1;a++){
+          if(obj[arr2[a][0]]==undefined){
+              obj[arr2[a][0]]=[arr2[a][1]]
+          }else{
+              obj[arr2[a][0]].push(arr2[a][1])
+          }
+    }
+    console.log(obj)
+    let brr=[];
+    for(let key in obj){
+        let x=+obj[key].length;
+        console.log(x)
+        let sum=0
+        for(let a=0;a<=x-1;a++){
+            sum=sum+obj[key][a]
+        }
+        brr.push([+key,sum])
+    }
+    console.log(brr);
+    
+    let crr=brr.sort(function(a,b){
+        return a[0]-b[0]
+    })
+    return (crr)
+    
+    
+};
