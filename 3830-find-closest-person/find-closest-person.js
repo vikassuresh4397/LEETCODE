f671614064021370cca94a57737aeb133167af5e
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2025-09-04 21:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (87.60%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let a,b,c;
+    if(x>=0 && z>=0){
+        a=Math.abs(z-x);
+    }else if(x<0 && z<0){
+        a=Math.abs(z-x);
+    }else if(x>=0 && z<0){
+       a=(-1*z)+x
+    }else if(x<0 && z>=0){
+        a=(-1*x)+z
+    }
+    
+    if(y>=0 && z>=0){
+        b=Math.abs(z-y);
+    }else if(y<0 && z<0){
+        b=Math.abs(z-y);
+    }else if(y>=0 && z<0){
+       b=(-1*z)+y
+    }else if(y<0 && z>=0){
+        b=(-1*y)+z
+    }
+    
+    if(a>b){
+        return 2
+    }else if(a<b){
+        return 1
+    }else{
+        return 0
+    }
+    
+};
