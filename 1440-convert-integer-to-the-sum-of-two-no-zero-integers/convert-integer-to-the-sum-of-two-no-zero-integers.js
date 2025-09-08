75b0e8c137fb77cccee2144aff39ff205c460f76
Vikas Suresh <112813417+vikassuresh4397@users.noreply.github.com>
2025-09-08 13:27:40 +0530

    Time: 4 ms (14.49%) | Memory: 54.6 MB (23.19%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -4,21 +4,33 @@
  */
 var getNoZeroIntegers = function(n) {
     
- for(let a=1;a<=n;a++){
-     
-     let x=a+"";
-     let y=n-a;
-     y=y+"";
-     
-     if(x.includes("0")||y.includes("0")){
-         continue;
-     }else{
-         x=Number(x);
-         y=Number(y);
-         return [x,y]
-     }
-     
- }
-    
-    
+  
+  let obj={};
+  
+  let z=Math.floor(n/2);
+  console.log(z)
+  
+  for(let a=1;a<=z;a++){
+      let x=a,y=n-a;
+      let r=x+"",s=y+"";
+      let count=0,bount=0;
+      for(let b=0;b<=r.length-1;b++){
+          if(r[b]=="0"){
+              count++;
+          }
+      }
+      
+       for(let b=0;b<=s.length-1;b++){
+          if(s[b]=="0"){
+              bount++;
+          }
+      }
+      
+      if(count==0&&bount==0){
+          return [x,y]
+      }
+  }
+   
+  
+  
 };
